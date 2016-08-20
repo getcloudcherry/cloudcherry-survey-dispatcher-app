0a387f5ce339ada865491f5afc112bd35e1ef73f
MaheshKousik <kousikres@gmail.com>
2016-08-20 17:50:32 +0530

    Source Files for CloudCherry Dispatcher App

---
+++ ImportCloud.cs
@@ -0,0 +1,133 @@
+﻿using CloudCherry;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+namespace DispatcherScheduler
+{
+    class CloudCherry
+    {
+        ScheduledService objservice = new ScheduledService();
+        DispatcherHelper objhelper = new DispatcherHelper();
+
+        public async void ImportData()
+        {
+            DispatcherConfig objDispatcher = new DispatcherConfig();
+            try
+            {
+                //Recording log 
+                objservice.TraceService("Deserializing ");
+
+                List<DispatcherConfig> objDispatcherlist = objhelper.GetDispatcherlist();//Deserialising the content in  the Config.json file               
+                string CSVstring = "";
+                objDispatcher = objDispatcherlist[0];
+                if (!File.Exists(objDispatcher.InputSource))
+                {
+                    File.Delete(ScheduledService.schedularpath);
+                    return;
+                }
+                if (!objhelper.ValidatingFileTypes(objDispatcher))
+                {
+                    return;
+
+                }
+                //List of comma separated strings which holds data that needs to be bulk uploaded using survey token
+                List<string> CSVstringarr = new List<string>();
+
+                //Initiating CloudCherry API Endpoint with CloudCherryAPIEndPoint, CloudCherryAccount, CloudCherrySecret Specified in config.json
+                APIClient client = new APIClient(objDispatcher.CloudCherryAPIEndPoint, objDispatcher.CloudCherryAccount, objDispatcher.CloudCherrySecret);
+
+                //Authenticating user using CloudCherry API
+                if (!await client.Login())
+                {
+                    //CloudCherry Authentication failed writing to Log
+                    objservice.TraceService("Login Failed");
+                    return;
+                }
+
+                //Retreiving all the active questions for the user for validating with the questions given the Input(Excel/CSV)
+                List<Question> Ques = await client.GetQuestions(true);
+
+                //Writing in to the log what Input type was selected for bulk import of tokens
+                objservice.TraceService("Input Type :" + objDispatcher.InputFileType);
+
+                //Retreiving comma separated string of bu
+                CSVstring = objhelper.GetCSVString(objDispatcher);
+
+                //Validating the prefill questions  in input files(excel or csv)
+                if (!objhelper.ValidatingPrefillQuestions(CSVstring, Ques))
+                {
+                    objservice.TraceService("Invalid questions encountered");
+                    return;
+                }
+
+
+                CSVstringarr = objhelper.SplitsCSVstring(CSVstring);
+                if (CSVstringarr.Count == 0)
+                {
+                    objservice.TraceService("No answers found");
+                    return;
+                }
+
+                for (int a = 0; a < CSVstringarr.Count; a++)
+                {
+
+                    objservice.TraceService("Uploading Bulk tokens " + (a + 1) + " of " + CSVstringarr.Count);
+                    //Bulk Uploading csv string with number of rows <=50000 at a time
+                    string Outputresponse = await client.UploadBulkTokens(CSVstringarr[a], objDispatcher.SurveyValidFor, objDispatcher.SurveyUses, objDispatcher.SurveyLocation);
+                    if (Outputresponse == null)
+                    {
+                        objservice.TraceService("Invalid data encountered");
+                        return;
+                    }
+                    objservice.TraceService("Output Type :" + objDispatcher.OutputType);
+                    if (objDispatcher.OutputType == "CSV")
+                    {//Writing response to CSV file in local folder or ftp folder
+                        objhelper.WriteCSV(objDispatcher, Outputresponse, a == 0);
+                    }
+
+                    if (objDispatcher.OutputType == "Email")
+                    {//Sending  emails
+                        objhelper.SendEmail(Ques, Outputresponse, objDispatcher);
+
+                    }
+                    if (objDispatcher.OutputType == "SMS")
+                    {//Sending smses
+                        objhelper.SendSMS(Ques, Outputresponse, objDispatcher);
+                    }
+                }
+
+
+            }
+            catch (Exception ee)
+            {
+                objservice.TraceService(ee.Message);
+            }
+            finally
+            {
+              
+                if (File.Exists(objDispatcher.InputSource))
+                {
+                    objservice.TraceService("completed");
+                    string CompletedFilePath = ConfigurationManager.AppSettings["CompletedFilePath"];
+                    CompletedFilePath = CompletedFilePath + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Year + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second + @"\";
+                    Directory.CreateDirectory(CompletedFilePath);
+                    CompletedFilePath = CompletedFilePath + Path.GetFileName(objDispatcher.InputSource);
+                    File.Move(objDispatcher.InputSource, CompletedFilePath);
+                    File.Delete(objDispatcher.InputSource);
+                }
+            }
+        }
+    }
+}
---
+++ ScheduledService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+using System.Configuration;
+
+namespace DispatcherScheduler
+{
+    public partial class ScheduledService : ServiceBase
+    {
+        Timer timer = new Timer();
+        public ScheduledService()
+        {
+            InitializeComponent();
+        }
+      public  static string schedularpath = "";
+        protected override void OnStart(string[] args)
+        {
+
+            //handle Elapsed event
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+
+            //This statement is used to set interval to serviceInterval specified in app.config
+
+            timer.Interval = Convert.ToDouble(ConfigurationManager.AppSettings["serviceInterval"].ToString());
+
+            //enabling the timer
+            timer.Enabled = true;
+            
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            //TraceService("Another entry at cc " + DateTime.Now);
+
+            string logFilePath = ConfigurationManager.AppSettings["logFilePath"];
+            if (!Directory.Exists(logFilePath))
+            {
+                return;
+            }
+            schedularpath = logFilePath + "DiapatcherAppLog_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + "";
+            if (!Directory.Exists(schedularpath))
+                Directory.CreateDirectory(schedularpath);
+            string filepath = schedularpath + @"\" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + "_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute + "_" + DateTime.Now.Second + ".log";
+            schedularpath = filepath;
+            string JsonConfigPath = ConfigurationManager.AppSettings["JsonConfigPath"];
+            if (!File.Exists(JsonConfigPath))
+            {
+                TraceService("Json file doesnot exist : " + JsonConfigPath);
+                return;
+            }
+            TraceService("service started");//service starts
+            CloudCherry objcloud = new CloudCherry();
+            objcloud.ImportData();//function to import data into CloudCherry
+        }
+        protected override void OnStop()
+        {
+            stopTimer();
+        }
+
+        public void stopTimer()
+        {
+            if (File.Exists(schedularpath))
+            TraceService("service stopped");
+        }
+
+        //TraceService -- Method used to log activities of the dispatcher app service
+        public void TraceService(string content)
+        {
+            FileStream fs = new FileStream(schedularpath, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+            sw.WriteLine("(" + DateTime.Now + ") " + content);
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
